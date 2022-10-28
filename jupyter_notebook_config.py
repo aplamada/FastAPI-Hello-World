819daa65f4e155d547533090153aba5a7c2c99b7
Andrei V. Plamada <andrei.plamada@uzh.ch>
2022-10-28 16:12:07 +0200

    binder compatible

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,12 @@
+# Configuration file for jupyter-notebook.
+c.ServerProxy.servers = {
+    'uvicorn': {
+        'command': [
+            'uvicorn',
+            'main:app',
+            '--port',
+            '{port}',
+        ],
+        'absolute_url': False,
+    }
+}
